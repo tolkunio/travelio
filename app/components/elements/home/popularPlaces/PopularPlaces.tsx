c3ab190e8f3c4b08c35fccb70701f2c4632af977
Tolkun Omurbekova <tolkun3922@gmail.com>
2024-02-16 16:44:57 +0600

    -getStaticProps, закончила оформить страницу 'популярные места'

--- app/components/elements/home/popularPlaces/PopularPlaces.tsx
+++ app/components/elements/home/popularPlaces/PopularPlaces.tsx
@@ -2,25 +2,26 @@ import s from './PopularPlaces.module.scss';
 import {FC} from "react";
 import {IPlace} from "@/app/type/place.type";
 import Link from "next/link";
+
 interface IPopularPlaces {
-    places:IPlace[]
+    places: IPlace[]
 }
 
-const PopularPlaces:FC<IPopularPlaces> = ({places}) => {
+const PopularPlaces: FC<IPopularPlaces> = ({places}) => {
     return (
         <div className={s.popularPlaces}>
             <h2>Popular places</h2>
             {
-                places.map(place=>(
-                <Link key={place.slug} href={`/place/${place.slug}`}
-                     className={s.place}
-                     style={{backgroundImage:`url('${place.imagePath}')`}}>
-                    {place.slug}
-                    <div className={s.heading}>
-                        {place.location.city+', '+place.location.country}
-                    </div>
-                </Link>
-            ))
+                places.map(place => (
+                    <Link key={place.slug} href={`/place/${place.slug}`}
+                          className={s.place}
+                          style={{backgroundImage: `url('${place.imagePath}')`}}>
+                        {place.slug}
+                        <div className={s.heading}>
+                            {place.location.city + ', ' + place.location.country}
+                        </div>
+                    </Link>
+                ))
             }
         </div>
     );
